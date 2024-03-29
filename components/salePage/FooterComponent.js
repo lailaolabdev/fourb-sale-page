f8c34f71d24abd6e9fc7b0d731122df2ecbc59a6
mac <mac@MacBook-Pro-khxng-mac.local>
2024-03-29 14:38:37 +0700

    fixed

--- components/salePage/FooterComponent.js
+++ components/salePage/FooterComponent.js
@@ -3,6 +3,7 @@ import { CORLOR_APP, CORLOR_WHITE } from "../../helper";
 import React from "react";
 import { useRouter } from "next/router";
 import { URL_PACKAGE_SYSTEM } from "../../const";
+import Marquee from "react-fast-marquee";
 const versionWeb = require("../../package.json");
 
 export default function FooterComponent() {
@@ -11,9 +12,15 @@ export default function FooterComponent() {
 
   return (
     <div className="footer-sale-page">
+      <div className="card-slo-can">
+        <Marquee speed={60} className="my-marquee">
+          <h1>"ໃຫ້ການຂາຍອອນລາຍ ເປັນເລື່ອງງ່າຍດ້ວຍ 4B"</h1>
+        </Marquee>
+      </div>
       <div
         className="footer-first"
-        style={{ padding: width > 700 ? "1em 3em" : "1em" }}>
+        style={{ padding: width > 700 ? "1em 3em" : "1em" }}
+      >
         <div className="footer-title">
           <div className="logo-main">
             <img
@@ -39,8 +46,12 @@ export default function FooterComponent() {
             <ol>
               {/* <li onClick={()=> navigate.back()}>ໜ້າຫລັກ ເຊວເພຈ</li> */}
               {/* <li>ວິທີການສັ່ງຊື້</li> */}
-              <li onClick={() => navigate.push("/policy")}><u>ນະໂຍບາຍການນຳໃຊ້</u></li>
-              <li onClick={() => window.open(URL_PACKAGE_SYSTEM)}><u>ແພັກເກັດລະບົບ</u></li>
+              <li onClick={() => navigate.push("/policy")}>
+                <u>ນະໂຍບາຍການນຳໃຊ້</u>
+              </li>
+              <li onClick={() => window.open(URL_PACKAGE_SYSTEM)}>
+                <u>ແພັກເກັດລະບົບ</u>
+              </li>
               {/* <li>ຕິດຕໍ່ພວກເຮົາ</li> */}
             </ol>
           </div>
@@ -49,24 +60,52 @@ export default function FooterComponent() {
               <b>ຟີເຈີີຂອງ 4B</b>
             </p>
             <ul>
-              <li style={{cursor:'pointer'}} onClick={()=> window.open("https://shop.bbbb.com.la")}><u>4B Live</u></li>
-              <li style={{cursor:'pointer'}} onClick={()=> window.open("https://pos.bbbb.com.la")}><u>4B POS</u></li>
-              <li style={{cursor:'pointer'}} onClick={()=> window.open("https://affiliate.bbbb.com.la")}><u>4B Affiliate</u></li>
-              <li style={{cursor:'pointer'}} onClick={()=> window.open("https://sp.bbbb.com.la")}><u>4B Sale Page</u></li>
-              
+              <li
+                style={{ cursor: "pointer" }}
+                onClick={() => window.open("https://shop.bbbb.com.la")}
+              >
+                <u>4B Live</u>
+              </li>
+              <li
+                style={{ cursor: "pointer" }}
+                onClick={() => window.open("https://pos.bbbb.com.la")}
+              >
+                <u>4B POS</u>
+              </li>
+              <li
+                style={{ cursor: "pointer" }}
+                onClick={() => window.open("https://affiliate.bbbb.com.la")}
+              >
+                <u>4B Affiliate</u>
+              </li>
+              <li
+                style={{ cursor: "pointer" }}
+                onClick={() => window.open("https://sp.bbbb.com.la")}
+              >
+                <u>4B Sale Page</u>
+              </li>
             </ul>
           </div>
         </div>
       </div>
       <div
         className="footer-second"
-        style={{ padding: width > 700 ? "1em 3em" : "1em" }}>
+      >
         <div>
           <h4>ປະເພດການຊຳລະ</h4>
           <div style={{ display: "flex", gap: 10 }}>
-            <img src="/assets/images/bcel_one.png" style={{ width: 45,height:45 }} />
-            <img src="/assets/images/jdbIco.png" style={{ width: 40,height: 40 }} />
-            <img src="/assets/images/indochina.png" style={{ width: 40, height:40 }} />
+            <img
+              src="/assets/images/bcel_one.png"
+              style={{ maxWidth: 45, maxHeight: 45 }}
+            />
+            <img
+              src="/assets/images/jdbIco.png"
+              style={{ maxWidth: 40, maxHeight: 40 }}
+            />
+            <img
+              src="/assets/images/indochina.png"
+              style={{ maxWidth: 40, maxHeight: 40 }}
+            />
           </div>
         </div>
         <div>
@@ -77,14 +116,16 @@ export default function FooterComponent() {
                 display: "flex",
                 justifyContent: "start",
                 alignItems: "center",
-              }}>
+              }}
+            >
               <div
                 style={{
-                  width: 30,
-                  height: 30,
+                  maxWidth: 30,
+                  maxHeight: 30,
                   borderRadius: "50%",
                   overflow: "hidden",
-                }}>
+                }}
+              >
                 <img
                   src="/assets/images/halLogo.png"
                   style={{ width: "100%" }}
@@ -98,18 +139,17 @@ export default function FooterComponent() {
                 display: "flex",
                 justifyContent: "start",
                 alignItems: "center",
-              }}>
+              }}
+            >
               <div
                 style={{
-                  width: 30,
-                  height: 30,
+                  maxWidth: 30,
+                  maxHeight: 30,
                   borderRadius: "50%",
                   overflow: "hidden",
-                }}>
-                <img
-                  src="/assets/images/mixay.png"
-                  style={{ width: "100%" }}
-                />
+                }}
+              >
+                <img src="/assets/images/mixay.png" style={{ width: "100%" }} />
               </div>
               {/* &nbsp;
               <small>ບໍລິສັດ ຂົນສົ່ງ ອານຸສິດ (ກຳລັງພັດທະນາ)</small> */}
@@ -122,43 +162,46 @@ export default function FooterComponent() {
             <div
               onClick={() => {
                 window.open("https://www.facebook.com/LailaoCF");
-              }}>
+              }}
+            >
               <img
                 src="https://www.facebook.com/images/fb_icon_325x325.png"
-                style={{ width: 30 }}
+                style={{ maxWidth: 30 }}
               />
             </div>
             <div
               onClick={() => {
                 window.open("https://www.youtube.com/@cflivebylailaocf2342");
-              }}>
+              }}
+            >
               <img
                 src="https://cdn-icons-png.flaticon.com/512/1384/1384060.png"
-                style={{ width: 30 }}
+                style={{ maxWidth: 30 }}
               />
             </div>
             <div
               onClick={() => {
                 window.open("https://www.tiktok.com/@4bforbusiness");
-              }}>
+              }}
+            >
               <img
                 src="https://play-lh.googleusercontent.com/Ui_-OW6UJI147ySDX9guWWDiCPSq1vtxoC-xG17BU2FpU0Fi6qkWwuLdpddmT9fqrA"
-                style={{ width: 30 }}
+                style={{ maxWidth: 30 }}
               />
             </div>
             {/* <img src="/assets/images/bcelOne.png" style={{ width: 30 }} /> */}
           </div>
         </div>
-      </div>
 
-      <div
-        style={{
-          textAlign: "center",
-          background: CORLOR_APP,
-          color: CORLOR_WHITE,
-        }}>
-        {versionWeb?.version}
+        <div className="card-copy-right">
+          <p>© 4B, All Right Reserved | By Lailaolab</p>
+          {versionWeb?.version}
+        </div>
+
       </div>
+      {/* <p style={{display:'flex',background:'none',border:'1px solid red', width:'100%', justifyContent:'center', alignItems:'center', padding:'3em 0'}}>
+
+        </p>  */}
     </div>
   );
 }
