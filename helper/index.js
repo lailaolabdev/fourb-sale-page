ec829d545549c1848987bb0d6f091db52f5dfeb2
mac <mac@MacBook-Pro-khxng-mac.local>
2024-05-03 13:18:49 +0700

    fixed uri

--- helper/index.js
+++ helper/index.js
@@ -35,12 +35,12 @@ export const numberFormat = (_number) => {
 
 // ======================================= link upoint ================================
 // Dev
-export const SERVER_URI = "https://cf-dev-api.lailaolab.com/";
-export const SOCKET_SERVER_URI = "wss://cf-dev-api.lailaolab.com/";
+// export const SERVER_URI = "https://cf-dev-api.lailaolab.com/";
+// export const SOCKET_SERVER_URI = "wss://cf-dev-api.lailaolab.com/";
 
 // Production
-// export const SERVER_URI = "https://cf-api-dev.lailaolab.com/";
-// export const SOCKET_SERVER_URI = "wss://cf-api-dev.lailaolab.com/";
+export const SERVER_URI = "https://api.bbbb.com.la/";
+export const SOCKET_SERVER_URI = "wss://api.bbbb.com.la/";
 
 // localhost
 // export const SERVER_URI = "http://localhost:7070/";
