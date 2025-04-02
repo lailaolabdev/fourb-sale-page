80b1b523c982eb1096845370052e1441e96408ca
chii199 <lailaolabdev@gmail.com>
2025-04-02 11:21:27 +0700

    fix id shop on payment

--- pages/cartdetail/index.js
+++ pages/cartdetail/index.js
@@ -12,7 +12,7 @@ import {
   updateCartItemQuantity,
 } from "../../redux/salepage/cartReducer";
 import { setOrders } from "../../redux/setOrder/orders";
-import { useLazyQuery } from "@apollo/client";
+import { useLazyQuery, useMutation, useSubscription } from "@apollo/client";
 import { useRouter } from "next/router";
 import { GET_EXCHANGRATE } from "../../apollo/exchanrage";
 import {
@@ -29,9 +29,13 @@ import CustomButton from "@/components/CustomButton";
 import CustomNavbar from "@/components/CustomNavbar";
 import Alert from 'react-bootstrap/Alert';
 import { IoClose } from "react-icons/io5";
-import { Button, Breadcrumb } from "react-bootstrap"
+import { Button, Modal } from "react-bootstrap"
 import { MdHome } from "react-icons/md";
 import { RxSlash } from "react-icons/rx";
+import { CREATE_PAYMENT_LINK_WITH_PHAPAY } from "@/apollo/order/mutation";
+import { ON_RECEIVE_PAYMENT_LINK } from "@/apollo/payment/mutation";
+import { GET_ORDERGROUPS_WITH_SALEPAGE } from "@/apollo/order/query";
+import Invoice from "@/components/invoices/Invoice";
 
 
 export default function CartDetail() {
@@ -55,14 +59,27 @@ export default function CartDetail() {
   const { cartList } = useSelector((state) => state?.salepage);
   const { patchBack } = useSelector((state) => state?.setpatch);
 
-
   const [priceToPay, setPriceToPay] = useState();
   const [state, setState] = useState("idle");
+  const [showPreview, setShowPreview] = useState(false);
+  const [qrCodeUrl, setQrCodeUrl] = useState();
+  const [preTransactionId, setPreTransactionId] = useState("");
 
   const [showConfirmRemove, setShowConfirmRemove] = React.useState(false);
   const [cartDatas, setCartDatas] = useState([]);
   const [shopInfo, setShopInfo] = useState()
   const handleCloseRemoveProduct = () => setShowConfirmRemove(false);
+
+  // apollo client hooks
+  const [createOrderSalepage, { loading: loadingPayment }] = useMutation(CREATE_PAYMENT_LINK_WITH_PHAPAY);
+  const [loadOrderGroupWithSalepage, { data: orderGroupWithSalepage, loading: loadingOrdeWithSalepage }] = useLazyQuery(GET_ORDERGROUPS_WITH_SALEPAGE, { fetchPolicy: 'cache-and-network' })
+
+  const { data: onSubscriptionPaymentLink } = useSubscription(ON_RECEIVE_PAYMENT_LINK, {
+    variables: {
+      "transactionId": preTransactionId,
+    },
+  });
+
   const handleShowConfirmProduct = () => {
     setShowConfirmRemove(true);
   };
@@ -72,12 +89,49 @@ export default function CartDetail() {
     0
   );
 
-  // useEffect(()=> {
-  //   let shopData = JSON.parse(localStorage.getItem("SP_SHOP_DATA"))
-  //   console.log({shopData})
+  function extractTransactionId(url) {
+    try {
+      // Add protocol if missing
+      const fullUrl = url.startsWith('http') ? url : `https://${url}`;
 
-  // },[])
+      // Create URL object
+      const urlObj = new URL(fullUrl);
 
+      // Extract linkCode
+      const linkCode = urlObj.searchParams.get('linkCode');
+
+      return linkCode;
+    } catch (error) {
+      console.error('Error parsing URL:', error);
+      return '';
+    }
+  }
+
+  useEffect(() => {
+    if (qrCodeUrl) {
+      let transactionId = extractTransactionId(qrCodeUrl);
+      setPreTransactionId(transactionId)
+    }
+  }, [qrCodeUrl]);
+
+  useEffect(() => {
+    if (onSubscriptionPaymentLink) {
+      if (onSubscriptionPaymentLink?.onSubReceiveLinkCodeWithSalePage?.transactionId === preTransactionId) {
+        setShowPreview(false)
+        dispatch(setOrders([]));
+        dispatch(removeCartItem());
+
+        // load order group with salepage
+        loadOrderGroupWithSalepage({
+          variables: {
+            where: {
+              transactionId: onSubscriptionPaymentLink?.onSubReceiveLinkCodeWithSalePage?.transactionId,
+            },
+          },
+        })
+      }
+    }
+  }, [onSubscriptionPaymentLink]);
 
   useEffect(() => {
     if (patchBack?.id) {
@@ -88,9 +142,16 @@ export default function CartDetail() {
     }
 
     let shopData = JSON.parse(localStorage.getItem("SP_SHOP_DATA"))
-    if(shopData) {
+    if (shopData) {
       setShopInfo(shopData?.shop)
     }
+    //  loadOrderGroupWithSalepage({
+    //   variables: {
+    //     where: {
+    //       transactionId: "6ca27f9b-dd30-41d4-87ac-2eaa2de6f134",
+    //     },
+    //   },
+    // })
   }, [patchBack, cartList]);
 
   const handleConfirmCart = () => {
@@ -141,20 +202,70 @@ export default function CartDetail() {
   };
 
   const onBackPage = () => {
-      // Retrieve the state from local storage
-      const idPreState = JSON.parse(localStorage.getItem("PATCH_KEY"));
-  
-      // Construct the destination path based on available data
-      let destinationPath = `../shop/${idPreState?.id}`;
-  
-      if (idPreState?.affiliateId) {
-        destinationPath += `?affiliateId=${idPreState.affiliateId}`;
-        if (idPreState?.commissionForShopId) {
-          destinationPath += `&commissionForShopId=${idPreState.commissionForShopId}`;
-        }
+    // Retrieve the state from local storage
+    const idPreState = JSON.parse(localStorage.getItem("PATCH_KEY"));
+
+    // Construct the destination path based on available data
+    let destinationPath = `../shop/${idPreState?.id}`;
+
+    if (idPreState?.affiliateId) {
+      destinationPath += `?affiliateId=${idPreState.affiliateId}`;
+      if (idPreState?.commissionForShopId) {
+        destinationPath += `&commissionForShopId=${idPreState.commissionForShopId}`;
       }
-      router.replace(destinationPath);
+    }
+    router.replace(destinationPath);
   };
+
+  const onPaymentWithPhapay = async () => {
+    try {
+      // prepare orders
+      const _convertedOrders = await (cartList || []).map((order) => ({
+        stock: order?.id,
+        shop: patchBack?.id,
+        amount: order?.qty,
+        price: order?.price,
+        originPrice: order?.price,
+        productName: order?.name,
+        currency: order?.currency,
+        totalPrice: order?.qty * order?.price,
+      }));
+
+      // prepare orderGroup
+      let _orderGroup = {
+        shop: patchBack?.id,
+        sumPriceUsd: 0,
+        totalPrice: totalPrice,
+        sumPriceBaht: 0,
+        sumPrice: totalPrice,
+        type: "SALE_PAGE",
+        amount: cartDatas?.length,
+      };
+
+      if (loadingPayment) return;
+      const response = await createOrderSalepage({
+        variables: {
+          data: {
+            // amount: 1,
+            amount: totalPrice,
+            description: "4B_SALE_PAGE",
+            orders: _convertedOrders,
+            orderGroup: _orderGroup,
+          },
+        },
+      })
+
+      if (response) {
+        console.log("response: ", response)
+        setShowPreview(true);
+        let _preLink = JSON.parse(response?.data?.createPaymentLinkWithPhapay?.appLink);
+        setQrCodeUrl(_preLink?.redirectURL)
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 
   return (
@@ -164,123 +275,131 @@ export default function CartDetail() {
 
       <div style={{ padding: "5px 0" }}>
         <div className="bread-crumb">
-          <span onClick={onBackPage}><MdHome style={{fontSize:15}} /></span>
+          <span onClick={onBackPage}><MdHome style={{ fontSize: 15 }} /></span>
           <RxSlash />
-          <span style={{cursor:'default'}}>ກະຕ່າສິນຄ້າ</span>
+          <span style={{ cursor: 'default' }}>ກະຕ່າສິນຄ້າ</span>
         </div>
       </div>
-
-      {cartDatas?.length > 0 ? (
-        <div className="p-4">
-          <div className="w-100">
-            {cartDatas?.map((data, index) => {
-              return (
-                <div key={data?.id} className="cartItem-product">
-                  <div
-                    className="remove-single-item"
-                    onClick={() => dispatch(removeSingleItem(index))}
-                  >
-                    <IoClose
-                      style={{ fontSize: 20, cursor: "pointer" }}
-                    />
-                  </div>
-
-                  <div className="cartImage">
-                    {data?.image?.length > 0 ? (
-                      <img src={S3_URL + data?.image} alt="productImage" />
-                    ) : (
-                      <EmptyImage />
-                    )}
-                  </div>
-                  <div className="action-item">
-                    <h5>
-                      {index + 1}. {data?.name}
-                    </h5>
-                    <div className="amounts-and-qty">
-                      <h6>{numberFormat(data?.price)} ກີບ</h6>
-                      <h6>
-                        {isNaN(data?.price * data?.qty)
-                          ? 0
-                          : numberFormat(data?.price * data?.qty)}{" "}
-                        ກີບ
-                      </h6>
-                    </div>
-
-                    <div className="action-button">
-                      {data?.qty >= 2 ? (
-                        <div
-                          className="decrement"
-                          onClick={() => dispatch(decrementCartItem(index))}
-                        >
-                          <IoMdRemove />
-                        </div>
-                      ) : (
-                        <div className="decrement">
-                          <IoMdRemove />
-                        </div>
-                      )}
-
-                      <input
-                        type="number"
-                        className="amount-product"
-                        value={data?.qty}
-                        onChange={(e) => {
-                          const newQty = parseInt(e?.target?.value);
-                          if (data?.qty >= data?.amount) {
-                            message.error("ຈຳນວນທີຊື້ຫຼາຍກວ່າໃນສະຕ໋ອກແລ້ວ");
-                            return;
-                          }
-                          dispatch(
-                            updateCartItemQuantity({
-                              index,
-                              newQuantity: newQty,
-                            })
-                          );
-                        }}
-                      />
+      {orderGroupWithSalepage?.getOrderGroupWithSalePage?.id ? (
+        <div>
+          <Invoice invoiceData={orderGroupWithSalepage?.getOrderGroupWithSalePage} />
+        </div>
+      ) : (
+        <>
+          {cartDatas?.length > 0 ? (
+            <div className="p-4">
+              <div className="w-100">
+                {cartDatas?.map((data, index) => {
+                  return (
+                    <div key={data?.id} className="cartItem-product">
                       <div
-                        className="increment"
-                        onClick={() => {
-                          if (data?.qty >= data?.amount) {
-                            message.error("ຈຳນວນທີຊື້ຫຼາຍກວ່າໃນສະຕ໋ອກແລ້ວ");
-                            return;
-                          }
-                          dispatch(incrementCartItem(index));
-                        }}
+                        className="remove-single-item"
+                        onClick={() => dispatch(removeSingleItem(index))}
                       >
-                        <IoMdAdd />
+                        <IoClose
+                          style={{ fontSize: 20, cursor: "pointer" }}
+                        />
+                      </div>
+
+                      <div className="cartImage">
+                        {data?.image?.length > 0 ? (
+                          <img src={S3_URL + data?.image} alt="productImage" />
+                        ) : (
+                          <EmptyImage />
+                        )}
+                      </div>
+                      <div className="action-item">
+                        <h5>
+                          {index + 1}. {data?.name}
+                        </h5>
+                        <div className="amounts-and-qty">
+                          <h6>{numberFormat(data?.price)} ກີບ</h6>
+                          <h6>
+                            {isNaN(data?.price * data?.qty)
+                              ? 0
+                              : numberFormat(data?.price * data?.qty)}{" "}
+                            ກີບ
+                          </h6>
+                        </div>
+
+                        <div className="action-button">
+                          {data?.qty >= 2 ? (
+                            <div
+                              className="decrement"
+                              onClick={() => dispatch(decrementCartItem(index))}
+                            >
+                              <IoMdRemove />
+                            </div>
+                          ) : (
+                            <div className="decrement">
+                              <IoMdRemove />
+                            </div>
+                          )}
+
+                          <input
+                            type="number"
+                            className="amount-product"
+                            value={data?.qty}
+                            onChange={(e) => {
+                              const newQty = parseInt(e?.target?.value);
+                              if (data?.qty >= data?.amount) {
+                                message.error("ຈຳນວນທີຊື້ຫຼາຍກວ່າໃນສະຕ໋ອກແລ້ວ");
+                                return;
+                              }
+                              dispatch(
+                                updateCartItemQuantity({
+                                  index,
+                                  newQuantity: newQty,
+                                })
+                              );
+                            }}
+                          />
+                          <div
+                            className="increment"
+                            onClick={() => {
+                              if (data?.qty >= data?.amount) {
+                                message.error("ຈຳນວນທີຊື້ຫຼາຍກວ່າໃນສະຕ໋ອກແລ້ວ");
+                                return;
+                              }
+                              dispatch(incrementCartItem(index));
+                            }}
+                          >
+                            <IoMdAdd />
+                          </div>
+                        </div>
                       </div>
                     </div>
+                  );
+                })}
+
+                <div className="removeCart">
+                  <div></div>
+                  <div
+                    className="removeIcon2"
+                    onClick={() => handleShowConfirmProduct()}
+                  >
+                    <RiDeleteBin6Line style={{ fontSize: 20 }} />{" "}
+                    &nbsp;ຍົກເລີກສິນຄ້າທັງໝົດ
                   </div>
                 </div>
-              );
-            })}
-
-            <div className="removeCart">
-              <div></div>
-              <div
-                className="removeIcon2"
-                onClick={() => handleShowConfirmProduct()}
-              >
-                <RiDeleteBin6Line style={{ fontSize: 20 }} />{" "}
-                &nbsp;ຍົກເລີກສິນຄ້າທັງໝົດ
               </div>
-            </div>
-          </div>
 
-          <div className="action-cart-product-footer">
-            <div className="action-price-amounts">
-              <h5>ສິນຄ້າ:</h5>
-              <h5>{cartDatas?.length} ລາຍການ</h5>
-            </div>
-            <br />
-            <div className="action-price-amounts">
-              <h5>ເງິນລວມ:</h5>
-              <h5>{isNaN(totalPrice) ? 0 : numberFormat(totalPrice)} ກີບ</h5>
-            </div>
-            <div className="paid-buy">
-              <Button style={{ background: CORLOR_APP, width: '100%' }} onClick={handleConfirmCart} >ຈ່າຍເງິນ</Button>
-              {/* <CustomButton
+              <div className="action-cart-product-footer">
+                <div className="action-price-amounts">
+                  <h5>ສິນຄ້າ:</h5>
+                  <h5>{cartDatas?.length} ລາຍການ</h5>
+                </div>
+                <br />
+                <div className="action-price-amounts">
+                  <h5>ເງິນລວມ:</h5>
+                  <h5>{isNaN(totalPrice) ? 0 : numberFormat(totalPrice)} ກີບ</h5>
+                </div>
+                <div className="paid-buy">
+                  {/* {patchBack?.id === "63ec6d38c617221e6ccf5edc" ? */}
+                  {patchBack?.id === "67d7c40e39135102068ccec6" ?
+                    <Button disabled={loadingPayment} style={{ background: CORLOR_APP, width: '100%' }} onClick={onPaymentWithPhapay} >{loadingPayment ? "ກຳລັງຊຳລະ..." : "ຊຳລະເງິນ"}</Button>
+                    : <Button style={{ background: CORLOR_APP, width: '100%' }} onClick={handleConfirmCart} >ຈ່າຍເງິນ</Button>}
+                  {/* <CustomButton
                 type="submit"
                 text="ສັ່ງຊື້"
                 state={state}
@@ -290,27 +409,35 @@ export default function CartDetail() {
                 padding={8}
                 width="100%"
               /> */}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      ) : (
-        <div style={{ padding: '2em', overflow: 'hidden', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-          <Alert style={{ width: "100%", textAlign: 'center' }} variant="warning">
-            <small>ກະລຸນາເລືອກສິນຄ້າກ່ອນ!</small>
-          </Alert>
-          <img style={{ width: '100%', maxWidth: 400, height: '100%' }} src="https://test.techhut.com.bd/images/no-item-in-cart.gif" />
-        </div>
+          ) : (
+            <div style={{ padding: '2em', overflow: 'hidden', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+              <Alert style={{ width: "100%", textAlign: 'center' }} variant="warning">
+                <small>ກະລຸນາເລືອກສິນຄ້າກ່ອນ!</small>
+              </Alert>
+              <img style={{ width: '100%', maxWidth: 400, height: '100%' }} src="https://test.techhut.com.bd/images/no-item-in-cart.gif" />
+            </div>
+          )}
+        </>
       )}
 
-      <ModalConfirmComponent
+      < ModalConfirmComponent
         showConfirmModal={showConfirmRemove}
-        handleCancel={handleCloseRemoveProduct}
-        handleConfirm={handleConfirmRemoveCart}
-        title="ແຈ້ງເຕືອນ"
-        text="ສິນຄ້າຢູ່ໃນກະຕ່າຂອງທ່ານຈະຖຶກລົບອອກທັງໝົດ?"
+      handleCancel={handleCloseRemoveProduct}
+      handleConfirm={handleConfirmRemoveCart}
+      title="ແຈ້ງເຕືອນ"
+      text="ສິນຄ້າຢູ່ໃນກະຕ່າຂອງທ່ານຈະຖຶກລົບອອກທັງໝົດ?"
       />
 
       <FooterComponent />
+
+      <Modal size="lg" centered show={showPreview} onHide={() => setShowPreview(false)}>
+        <Modal.Body style={{ width: '100%', height: "85vh", maxHeight: "70em", display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', position: 'relative' }}>
+          <iframe src={qrCodeUrl} title="description" style={{ width: '100%', height: '100%' }} />
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
